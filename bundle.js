c4a9316ad927e54c39931342481ff259d35bfa16
PiyushKoranne <94380016+PiyushKoranne@users.noreply.github.com>
2024-05-23 12:58:49 +0530

    Create bundle.js

---
+++ bundle.js
@@ -0,0 +1,8 @@
+(function() {
+  console.log("Script loaded");
+
+  var container = document.createElement('div');
+  container.id = 'carousel-container';
+  container.innerHTML = '<h2>This is my custom page section</h2>';
+  document.body.appendChild(container);
+})();
